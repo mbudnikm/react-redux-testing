140aaf823a3c493af04f10b7e5db89643b40e03b
mbudnikm <mbudnikm97@gmail.com>
2019-09-03 11:56:38 +0200

    create snapshot test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { exportAllDeclaration } from '@babel/types';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -12,4 +13,9 @@ it('should render without craching (with prop all=false)', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App all={false}/>, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it('should match snapshot', () => {
+  const app = <App />
+  expect(app).toMatchSnapshot()
 });
