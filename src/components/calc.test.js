dbdb71b0434125e1a642f49365a730a5c8598a43
mbudnikm <mbudnikm97@gmail.com>
2019-09-03 11:37:21 +0200

    add calc test

---
+++ src/components/calc.test.js
@@ -0,0 +1,9 @@
+describe('calc', () => {
+    it('should add two numbers', () => {
+        expect(2 * 2).toEqual(4)
+    });
+
+    it('should multiply two numbers', () => {
+        expect(2 * 2).toEqual(4)
+    });
+});
