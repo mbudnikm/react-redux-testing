fa268706b1f3e1f13a6aa630f9733da26bf0298d
mbudnikm <mbudnikm97@gmail.com>
2019-09-03 13:11:42 +0200

    add snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { exportAllDeclaration } from '@babel/types';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import renderer from "react-test-renderer";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('should render without craching (with prop all=false)', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App all={false}/>, div);
+it("should render without craching (with prop all=false)", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App all={false} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('should match snapshot', () => {
-  const app = <App />
-  expect(app).toMatchSnapshot()
+it("should match snapshot", () => {
+  const app = <App />;
+  expect(app).toMatchInlineSnapshot(`<App />`);
 });
